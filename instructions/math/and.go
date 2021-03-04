cfe1458c065be78c7edeeef568079d3ee9f846a6
Never <897013703@qq.com>
2021-03-04 18:47:18 +0800

    初步实现类型转化指令

---
+++ instructions/math/and.go
@@ -0,0 +1,26 @@
+package math
+
+import "jvmgo/instructions/base"
+import "jvmgo/rtda"
+
+type IAND struct {
+	base.NoOperandsInstruction
+}
+
+func (receiver *IAND) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	result := v1 & v2
+	stack.PushInt(result)
+}
+
+type LAND struct{ base.NoOperandsInstruction }
+
+func (receiver *LAND) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	result := v1 & v2
+	stack.PushLong(result)
+}

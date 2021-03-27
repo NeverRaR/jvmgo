a5a03056771a63eaa9c2827a84192c8c43244697
Never <897013703@qq.com>
2021-03-27 19:15:14 +0800

    完成本地方法注册逻辑

--- rtda/heap/method.go
+++ rtda/heap/method.go
@@ -29,18 +29,46 @@ func (receiver *Method) MaxLocal() uint {
 func newMethods(class *Class, cfMethods []*classfile.MemberInfo) []*Method {
 	methods := make([]*Method, len(cfMethods))
 	for i, cfMethod := range cfMethods {
-		methods[i] = &Method{}
-		methods[i].class = class
-		methods[i].copyMemberInfo(cfMethod)
-		methods[i].copyAttributes(cfMethod)
-		methods[i].calcArgSlotCount()
+		methods[i] = newMethod(class, cfMethod)
 	}
 	return methods
 }
 
-func (receiver *Method) calcArgSlotCount() {
-	parsedDescriptor := parseMethodDescriptor(receiver.descriptor)
-	for _, paramType := range parsedDescriptor.parameterTypes {
+func newMethod(class *Class, cfMethod *classfile.MemberInfo) *Method {
+	method := &Method{}
+	method.class = class
+	method.copyMemberInfo(cfMethod)
+	method.copyAttributes(cfMethod)
+	md := parseMethodDescriptor(method.descriptor)
+	method.calcArgSlotCount(md.parameterTypes)
+	if method.IsNative() {
+		method.injectCodeAttribute(md.returnType)
+	}
+	return method
+}
+
+func (receiver *Method) injectCodeAttribute(returnType string) {
+	receiver.maxStack = 4
+	receiver.maxLocal = receiver.argSlotCount
+	switch returnType[0] {
+	case 'V':
+		receiver.code = []byte{0xfe, 0xb1} //return
+	case 'D':
+		receiver.code = []byte{0xfe, 0xaf} //dreturn
+	case 'F':
+		receiver.code = []byte{0xfe, 0xae} //freturn
+	case 'J':
+		receiver.code = []byte{0xfe, 0xad} //lreturn
+	case 'L', '[':
+		receiver.code = []byte{0xfe, 0xb0} //areturn
+	default:
+		receiver.code = []byte{0xfe, 0xac} //ireturn
+
+	}
+}
+
+func (receiver *Method) calcArgSlotCount(paramTypes []string) {
+	for _, paramType := range paramTypes {
 		receiver.argSlotCount++
 		if paramType == "J" || paramType == "D" {
 			receiver.argSlotCount++
